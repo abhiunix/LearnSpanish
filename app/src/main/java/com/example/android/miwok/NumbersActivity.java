29bc338a0ea3a85f0cf5b574ac2369aead470b01
abhijeetsingh <abhi009215@gmail.com>
2018-06-09 15:30:38 +0530

    Lesson 40 - Add Text Views to the Layout Based on the List. (Without Loop)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,6 +18,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -43,15 +45,26 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("ten");
 
 
-        Log.v("NumbersActivity", "word at index 0" + ": " + words.get(0));
-        Log.v("NumbersActivity", "word at index 1" + ": " + words.get(1));
-        Log.v("NumbersActivity", "word at index 2: " + words.get(2));
-        Log.v("NummbersActivity","Word at index 3: " + words.get(3));
-        Log.v("NumbersActivity","Word at index 4: " + words.get(4));
-        Log.v("NumbersAcitivity","Word at index 5: " + words.get(5));
-        Log.v("NumbersAcitivity","Word at index 6: " + words.get(6));
-        Log.v("NumbersAcitivity","Word at index 7: " + words.get(7));
-        Log.v("NumbersAcitivity","Word at index 8: " + words.get(8));
-        Log.v("NumbersAcitivity","Word at index 9: " + words.get(9));
+       LinearLayout rootView =(LinearLayout)findViewById(R.id.rootView);
+
+       int index =0;
+
+       //Creating a new Object "TextView" and Storing it in a variable "wordView" who's dataType is TextView.(Context:this).
+       TextView wordView = new TextView(this);
+       wordView.setText(words.get(index));
+       rootView.addView(wordView);
+
+        index = index +1;
+
+        TextView wordView2 = new TextView(this);
+        wordView2.setText(words.get(index));
+        rootView.addView(wordView2);
+
+        index = index +1;
+
+        TextView wordView3 = new TextView(this);
+        wordView3.setText(words.get(index));
+        rootView.addView(wordView3);
+
     }
 }
