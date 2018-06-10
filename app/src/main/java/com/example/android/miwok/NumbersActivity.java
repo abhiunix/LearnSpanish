be43bfee5f9fb9ad700f494ef00b817c62124719
abhijeetsingh <abhi009215@gmail.com>
2018-06-10 14:47:04 +0530

    Lesson 54 - Updating to GridViews+ArrayAdapter. (Solution).

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -19,10 +19,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
@@ -40,7 +42,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("three");
         words.add("four");
         words.add("five");
-        words.add("six");
+        words.add("6");
         words.add("seven");
         words.add("eight");
         words.add("nine");
@@ -53,20 +55,20 @@ public class NumbersActivity extends AppCompatActivity {
         //this list item layout contains a single {@link TextView}, which the adapter will set to display
         //a single word.
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,words);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
         //Find the {@link ListView} object in the view hierarchy of the {@link Activity}
         //There should be a {@link ListView} with the view ID called list,
         //which is declared in activity_numbers.xml layout file.
 
-       ListView listView =(ListView) findViewById(R.id.list);
+       GridView gridView =(GridView) findViewById(R.id.grid);
 
        //Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
         //{@link listView} will display the list items for each word in the list of words.
         //Do this by calling the setAdapter method on the {@link ListView} and pass in
         //1 Argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
 
-        listView.setAdapter(itemsAdapter);
+        gridView.setAdapter(itemsAdapter);
 
        }
 
